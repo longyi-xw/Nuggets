1fc10cc6c4b55361d7d790942dfa2f7571136af2
longxiuwen <2691049525@qq.com>
2023-10-25 09:33:31 +0800

    fix: edit cookie

--- src/config.js
+++ src/config.js
@@ -4,35 +4,35 @@ let OPENAI_API_KEY = "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"
 let COOKIE_DEFAULT = require('./cookie').COOKIE
 let cookie_str
 try {
-    let cookie = COOKIE_DEFAULT;
-    if (typeof cookie === 'string') {
-        cookie = JSON.parse(cookie)
-        COOKIE_DEFAULT = cookie
-    }
-    cookie_str = Object.keys(cookie).map(key => `${key}=${cookie[key]}`).join(";")
+  let cookie = COOKIE;
+  if(typeof cookie === 'string') {
+    cookie = JSON.parse(cookie)
+    COOKIE_DEFAULT = cookie
+  }
+  cookie_str = Object.keys(cookie).map(key => `${key}=${cookie[key]}`).join(";")
 } catch (error) {
-    console.log("json 字符串格式错误！")
-    cookie_str = COOKIE_DEFAULT
+  console.log("json 字符串格式错误！")
+  cookie_str = COOKIE_DEFAULT
 }
 const headers = {
-    'content-type': 'application/json; charset=utf-8',
-    'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/111.0",
-    "Accept": "*/*",
-    "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-    "Authorization": "Bearer " + OPENAI_API_KEY,
-    referer: 'https://juejin.cn/',
-    cookie: cookie_str
+  'content-type': 'application/json; charset=utf-8',
+  'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/111.0",
+  "Accept": "*/*",
+  "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+  "Authorization": "Bearer " + OPENAI_API_KEY,
+  referer: 'https://juejin.cn/',
+  cookie: cookie_str
 };
 console.log("用户邮箱：", EMAIL_USER)
 console.log("发送邮箱到：", EMAIL_TO)
 module.exports = {
-    headers,
-    user: EMAIL_USER,
-    pass: EMAIL_PASS,
-    to: EMAIL_TO,
-    uid: UID,
-    DD_BOT_TOKEN,
-    DD_BOT_SECRET,
-    WORKWX_WEBHOOK,
-    COOKIE_DEFAULT
+  headers,
+  user: EMAIL_USER,
+  pass: EMAIL_PASS,
+  to: EMAIL_TO,
+  uid: UID,
+  DD_BOT_TOKEN,
+  DD_BOT_SECRET,
+  WORKWX_WEBHOOK,
+  COOKIE_DEFAULT
 };
--- src/cookie.js
+++ src/cookie.js
@@ -1,14 +1,14 @@
 const COOKIE = {
-    "__tea_cookie_tokens_2608": "%257B%2522web_id%2522%253A%25227246965070184531495%2522%252C%2522user_unique_id%2522%253A%25227246965070184531495%2522%252C%2522timestamp%2522%253A1687315560289%257D",
-    "passport_csrf_token": "4ba55c83ad9f803ffbe48817bbc7f741",
-    "passport_csrf_token_default": "4ba55c83ad9f803ffbe48817bbc7f741",
-    "csrf_session_id": "313e7b3299f52f2e7c538b80afc8bec0",
-    "msToken": "Auvfmmhsxy6ndkepU95i0rBY2t3dyO4LpaFRjTK6QN54XxDJit7B5xu_VpVoM8QOIVdndcMrz0EKmWkXw0k4P2JrMd0QGn2STQmTqz01YviUXl-B8WVpvs1IJGDwlQ==",
-    "n_mh": "m4Nwod14ocYxZ8VeMr34-AMseNqHnd0sqChhin9oDYw",
-    "sid_guard": "165f21943064fd6d7de7341ab0a15c2c%7C1690875628%7C31536000%7CWed%2C+31-Jul-2024+07%3A40%3A28+GMT",
-    "sid_tt": "165f21943064fd6d7de7341ab0a15c2c",
-    "uid_tt": "265f8aa699848264f68e5381f1595040",
-    "uid_tt_ss": "265f8aa699848264f68e5381f1595040"
+    "__tea_cookie_tokens_2608": "",
+    "passport_csrf_token": "",
+    "passport_csrf_token_default": "",
+    "csrf_session_id": "",
+    "msToken": "",
+    "n_mh": "",
+    "sid_guard": "",
+    "sid_tt": "",
+    "uid_tt": "",
+    "uid_tt_ss": ""
 }
 
 module.exports = {
